bb8b98b4514e7be8c3d9491e972fdcb9dc8e6afb
pmutua <pmutua@live.com>
2017-10-09 20:16:01 +0300

    Add New and Create Action with views

--- db/migrate/20171009120520_create_messages.rb
+++ db/migrate/20171009120520_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
       t.string :title
-      t.text :desctiption
+      t.text :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20171009120520) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
-    t.text "desctiption"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
