d79a405a618550fc00c33902322bd65d6525024b
pmutua <pmutua@live.com>
2017-10-20 13:56:12 +0300

    Comments Model,Controller and View files

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-resources :messages
-root 'messages#index'
+
+# nest comments within messages
+	resources :messages do 
+		resources :comments
+	end
+
+	root 'messages#index'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020103343) do
+ActiveRecord::Schema.define(version: 20171020104335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "message_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["message_id"], name: "index_comments_on_message_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -40,4 +50,6 @@ ActiveRecord::Schema.define(version: 20171020103343) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "messages"
+  add_foreign_key "comments", "users"
 end
